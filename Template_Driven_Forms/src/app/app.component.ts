3a43f94e3dd947ab4553fa40698fa1967b37feeb
Vigneshwaran Thiruselvam <vthiruselvam@simplesolve.com>
2023-03-21 19:35:17 +0530

    Reactive-changes-2

--- Template_Driven_Forms/src/app/app.component.ts
+++ Template_Driven_Forms/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Template_Driven_Forms';
+export class AppComponent implements OnInit {
+  public tempDriForBtn = false;
+  public reaForBtn = false;
+  public forArrBtn = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.valueChange();
+  }
+
+  valueChange() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        if(event.url === '/Template-driven-form'){
+          this.tempDriForBtn = true;
+          this.reaForBtn = false;
+          this.forArrBtn = false;
+        }
+        if(event.url === '/Reactive-Form'){
+          this.tempDriForBtn = false;
+          this.reaForBtn = true;
+          this.forArrBtn = false;
+        }
+      });
+  }
 }
--- Template_Driven_Forms/src/app/reactive-form/reactive-form.component.ts
+++ Template_Driven_Forms/src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -19,6 +19,9 @@ export class ReactiveFormComponent implements OnInit {
     'Years',
     'Months'
   ];
+
+  public errorMessage: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -30,37 +33,100 @@ export class ReactiveFormComponent implements OnInit {
 
   formBuilder() {
     this.employeeReactiveForm = new FormGroup({
-      firstName: new FormControl(''),
-      lastName: new FormControl(''),
+      firstName: new FormControl('',[Validators.required, Validators.maxLength(15), Validators.pattern("^[a-zA-Z]+$")],),
+      lastName: new FormControl('',[Validators.required, Validators.maxLength(15), Validators.pattern("^[a-zA-Z]+$")]),
       address: new FormGroup({
-        address: new FormControl(''),
-        zipCode: new FormControl(),
-        city: new FormControl(''),
-        state: new FormControl(''),
-        country: new FormControl(''),
+        address: new FormControl('',[Validators.required]),
+        zipCode: new FormControl('',[Validators.required]),
+        city: new FormControl('',[Validators.required]),
+        state: new FormControl('',[Validators.required]),
+        country: new FormControl('',[Validators.required]),
       }),
-      gender: new FormControl(''),
-      dob: new FormControl(''),
-      age: new FormControl(),
-      email: new FormControl(''),
-      mNumber: new FormControl(),
-      jStatus: new FormControl(''),
-      experience: new FormControl(),
-      period: new FormControl('')
+      gender: new FormControl('',[Validators.required]),
+      dob: new FormControl('',[Validators.required]),
+      age: new FormControl('',[Validators.required]),
+      email: new FormControl('',[Validators.required, Validators.pattern('[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}')]),
+      mNumber: new FormControl('',[Validators.required, Validators.maxLength(10), Validators.minLength(1)]),
+      jStatus: new FormControl('',[Validators.required]),
+      experience: new FormControl('', [Validators.required]),
+      period: new FormControl('',[Validators.required]),
+      submitted: new FormControl('false')
     })
   }
 
-  onSubmit(employeeReactiveForm: any) {
-    console.log(employeeReactiveForm.value);
+  onSubmit() {
+    this.errorMessage = [];
+    this.employeeReactiveForm.get('submitted')?.patchValue(true);
+    if(this.employeeReactiveForm?.invalid){
+      if(this.employeeReactiveForm.get('firstName')?.errors?.['required']) {
+        this.errorMessage.push('*'+'First Name is required');
+      }
+      if(this.employeeReactiveForm.get('lastName')?.errors?.['required']) {
+        this.errorMessage.push('*'+'Last Name is required');
+      }
+      if(this.employeeReactiveForm.get('address')?.get('address')?.errors?.['required']) {
+        this.errorMessage.push('*'+'Address is required');
+      }
+      if(this.employeeReactiveForm.get('address')?.get('zipCode')?.errors?.['required']) {
+        this.errorMessage.push('*'+'Zip Code is required');
+      }
+      if(this.employeeReactiveForm.get('address')?.get('city')?.errors?.['required']) {
+        this.errorMessage.push('*'+'City is required');
+      }
+      if(this.employeeReactiveForm.get('address')?.get('state')?.errors?.['required']) {
+        this.errorMessage.push('*'+'State is required');
+      }
+      if(this.employeeReactiveForm.get('address')?.get('country')?.errors?.['required']) {
+        this.errorMessage.push('*'+'Country is required');
+      }
+      if(this.employeeReactiveForm.get('gender')?.errors?.['required']) {
+        this.errorMessage.push('*'+'Gender is required');
+      }
+      if(this.employeeReactiveForm.get('dob')?.errors?.['required']) {
+        this.errorMessage.push('*'+'Date of Birth is required');
+      }
+      if(this.employeeReactiveForm.get('age')?.errors?.['required']) {
+        this.errorMessage.push('*'+'Age is required');
+      }
+      if(this.employeeReactiveForm.get('email')?.errors?.['required']) {
+        this.errorMessage.push('*'+'Email is required');
+      }
+      if(this.employeeReactiveForm.get('email')?.errors?.['pattern']) {
+        this.errorMessage.push('*'+'Invalid Email Format');
+      }
+      if(this.employeeReactiveForm.get('mNumber')?.errors?.['required']) {
+        this.errorMessage.push('*'+'Mobile Number is required');
+      }
+      if(this.employeeReactiveForm.get('jStatus')?.errors?.['required']) {
+        this.errorMessage.push('*'+'Job Status is required');
+      }
+      if(this.employeeReactiveForm.get('experience')?.errors?.['required'] || this.employeeReactiveForm.get('period')?.errors?.['required']) {
+        this.errorMessage.push('*'+'If you in Work, Work Experience is Required.');
+      }
+    }
+    console.log(this.employeeReactiveForm);
   }
 
   onBlur() {
+    console.log(this.employeeReactiveForm.get("address")?.get("zipCode"))
     if (this.employeeReactiveForm.get("address")?.get("zipCode")?.value === 626001) {
       this.employeeReactiveForm.patchValue({
         address: {
           city: 'Virudhunagar',
           state: 'Tamilnadu',
           country: 'India'
+        }
+      })
+    }
+    else if (this.employeeReactiveForm.get("address")?.get("zipCode")?.value === null ||
+             this.employeeReactiveForm.get("address")?.get("zipCode")?.value === '' ||
+             this.employeeReactiveForm.get("address")?.get("zipCode")?.value === undefined ||
+             this.employeeReactiveForm.get("address")?.get("zipCode")?.value?.length != '00000' ) {
+      this.employeeReactiveForm.patchValue({
+        address: {
+          city: '',
+          state: '',
+          country: ''
         }
       })
     }
@@ -78,4 +144,27 @@ export class ReactiveFormComponent implements OnInit {
     })
   }
 
+  onReset() {
+    this.employeeReactiveForm.setValue({
+      firstName:'',
+      lastName: '',
+      address: {
+        address:'',
+        zipCode:'',
+        city: '',
+        state: '',
+        country:'',
+      },
+      gender: '',
+      dob: '',
+      age: '',
+      email: '',
+      mNumber: '',
+      jStatus: '',
+      experience:'',
+      period: '',
+      submitted: ''
+    })
+    this.errorMessage = [];
+  }
 }
